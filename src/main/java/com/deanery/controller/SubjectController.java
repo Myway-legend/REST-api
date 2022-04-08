817229edeb2c9cb97c4bc63b0984fc993fb2f97f
Myway-legend <kossamoilenko@gmail.com>
2022-04-08 18:34:05 +0300

    Update subject controller

--- src/main/java/com/deanery/controller/SubjectController.java
+++ src/main/java/com/deanery/controller/SubjectController.java
@@ -3,7 +3,10 @@ package com.deanery.controller;
 import com.deanery.entity.Subject;
 import com.deanery.service.SubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,28 +24,63 @@ public class SubjectController {
 
     @GetMapping(path = "/all")
     @ResponseBody
-    public List<Subject> getSubjects(@RequestParam(required = false) Optional<String> prefix) {
+    public ResponseEntity<List<Subject>> getSubjects(@RequestParam(required = false) Optional<String> prefix) {
 
-        return prefix.isPresent() ? subjectService.readSubjectsStartsWith(prefix.get()) : subjectService.readSubjects();
+        return prefix.isPresent() ?
+                new ResponseEntity<>(subjectService.readSubjectsStartsWith(prefix.get()), HttpStatus.OK) :
+                new ResponseEntity<>(subjectService.readSubjects(), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/all-ordered")
+    @ResponseBody
+    public ResponseEntity<List<Subject>> getSubjectsOrdered(@RequestParam(required = false) Optional<Object> asc) {
+
+        return asc.isPresent() ?
+                new ResponseEntity<>(subjectService.readSubjectsOrdered(true), HttpStatus.OK) :
+                new ResponseEntity<>(subjectService.readSubjectsOrdered(false), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/by-teacher")
+    @ResponseBody
+    public ResponseEntity<List<Subject>> getSubjectsByTeacher(@RequestParam Long id) {
+        try {
+            return new ResponseEntity<>(subjectService.readSubjectsByTeacher(id), HttpStatus.OK);
+        } catch (IllegalStateException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Invalid teacher");
+        }
     }
 
     @GetMapping
     @ResponseBody
-    public Subject getSubject(@RequestParam Long id) {
-        return subjectService.readSubjectById(id);
+    public ResponseEntity<Subject> getSubject(@RequestParam Long id) {
+        try {
+            return new ResponseEntity<>(subjectService.readSubjectById(id), HttpStatus.OK);
+        } catch (IllegalStateException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Subject not found");
+        }
     }
 
     @PostMapping
     public void postSubject(@RequestBody Subject subject) {
-        subjectService.createSubject(subject.getName());
+        try {
+            subjectService.createSubject(subject.getName());
+        } catch (IllegalStateException e) {
+            throw new ResponseStatusException(HttpStatus.FOUND, "Subject already exists");
+        }
+
     }
 
     @DeleteMapping
     public void deleteSubject(@RequestParam(required = false) Optional<Long> id) {
-        if (id.isPresent()) {
-            subjectService.deleteSubjectById(id.get());
-        } else {
-            subjectService.deleteAllSubjects();
+
+        try {
+            if (id.isPresent()) {
+                subjectService.deleteSubjectById(id.get());
+            } else {
+                subjectService.deleteAllSubjects();
+            }
+        } catch (IllegalStateException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Subject not found");
         }
     }
 
--- src/main/java/com/deanery/service/SubjectServiceImpl.java
+++ src/main/java/com/deanery/service/SubjectServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class SubjectServiceImpl implements SubjectService {
